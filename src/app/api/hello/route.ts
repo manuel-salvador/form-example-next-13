27be7839b91f5424a1833a71c4d0e882028bf0dd
Manuel Salvador <manu.sacr@hotmail.com>
2023-03-01 16:21:30 -0300

    form example

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,5 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+export async function POST(request: Request) {
+  const body = await request.json();
+  console.log(body);
+  return new Response('Hello, Next.js!');
 }
